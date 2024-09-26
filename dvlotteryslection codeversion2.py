dca2fe748e37b58b1ee8c8fab0750f3728d68312
Mohammad Hossain Rahmani <109447465+TaninRahmani@users.noreply.github.com>
2024-09-26 20:23:26 +0330

    Update dvlotteryslection codeversion2.py

--- dvlotteryslection codeversion2.py
+++ dvlotteryslection codeversion2.py
@@ -4,16 +4,24 @@ import winsound
 # Create a list of IDs A1 to A25000000
 x = ["A{}".format(i) for i in range(1, 25000001)]
 
+n=0
+while True:
+
 # Define the target ID
-target_id = "A21443463"
+ #target_id = random.sample(x,1)[0]
+ #is you are a couple or tow person, you can uncomment the following line and the if statement code
+ target_id1, target_id2 = random.sample(x, 2)
+
 
 # Sample 50000 random IDs without replacement
-selected_ids = random.sample(x, 50000)
+ selected_ids = random.sample(x, 50000)
+ n+=1
+
 
 # Check if the target ID is selected
-for y in selected_ids:
-    
-    if y == target_id:
-        print("Congratulations!")
-        winsound.Beep(540, 1000)
-        break
+ if target_id1 in selected_ids or target_id2 in selected_ids:
+
+ #if target_id in selected_ids:
+     print("Congratulations! you selected after: {} times of applying".format(n))
+     winsound.Beep(540, 1000)
+   
